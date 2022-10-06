72beb1a41d2c5ade9b83d87c903b34fd115505c5
koneki88 <114171959+koneki88@users.noreply.github.com>
2022-10-06 18:06:55 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	//case1
+	//int day;
+	//cin >> day;
+	//	switch (day) {
+	//		case 1: cout << "понедельник" << endl; break;
+	//		case 2: cout << "вторник" << endl; break;
+	//		case 3: cout << "среда" << endl; break;
+	//		case 4: cout << "четверг" << endl; break;
+	//		case 5: cout << "пятница" << endl; break;
+	//		case 6: cout << "суббота" << endl; break;
+	//		case 7: cout << "воскресенье" << endl; break;
+	//		case 8: cout << 0 << endl; break;
+	//	}
+
+
+ //case2
+	//int k;
+
+	//cout << "k = "; cin >> k;
+
+	//switch (k)
+	//		{
+	//		case 1: cout << "плохо"; break;
+	//		case 2: cout << "неудовлетворительно"; break;
+	//		case 3: cout << "удовлетворительно"; break;
+	//		case 4: cout << "хорошо"; break;
+	//		case 5: cout << "отлично"; break;
+	//		default: cout << "ошибка";
+	//		}
+	//			cout << endl << endl;
+
+//case3
+	//int a;
+	//cout << "Число:";
+	//cin >> a;
+	//
+	//		switch (a) {
+	//		case 1:
+	//		case 3:
+	//		case 5:
+	//		case 7:
+	//		case 8:
+	//		case 10:
+	//		case 12:
+	//			cout << "В месяце 31 днень";
+	//			break;
+	//		case 4:
+	//		case 6:
+	//		case 9:
+	//		case 11:
+	//			cout << "В месяце 30 днень";
+	//			break;
+	//		case 2:
+	//			cout << "В месяце 28 днень";
+	//			break;
+	//}
+//case4
+	//int a;
+	//cout << "Число:";
+	//cin >> a;
+	//			switch (a) {
+	//		case 1:
+	//		case 3:
+	//		case 5:
+	//		case 7:
+	//		case 8:
+	//		case 10:
+	//		case 12:
+	//			cout << "В месяце 31 днень";
+	//			break;
+	//		case 4:
+	//		case 6:
+	//		case 9:
+	//		case 11:
+	//			cout << "В месяце 30 днень";
+	//			break;
+	//		case 2:
+	//			cout << "В месяце 28 днень";
+	//			break;
+	//}
+	//case5
+	//int a, b, c;
+	//	cout << "Номер действия\n Сложение-1\n Вычитание-2\n Умножение-3\n Деление-4:";
+	//		cin >> a;
+	//		cout << "Число A:";
+	//	cin >> b;
+	//		cout << "Число B:";
+	//	cin >> c;
+	//			switch (a) {
+	//			case 1:
+	//			cout << b + c;
+	//				break;
+	//				case 2:
+	//				cout << b - c;
+	//				break;
+	//			case 3:
+	//				cout << b * c;
+	//			break;
+	//			case 4:
+	//			cout << b / c;
+	//				break;
+	//						}
+}
